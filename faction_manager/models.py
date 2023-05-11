644b41f75beed3f75821f0513971a43eedabe25a
David Alvarez <daveyalvarez03@gmail.com>
2023-05-11 16:48:01 -0700

    add factions and districts

--- faction_manager/models.py
+++ faction_manager/models.py
@@ -1,32 +1,35 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Faction(models.Model):
     name = models.CharField(max_length=100)
+    tier = models.IntegerField(default=0)
+    HOLD_STRENGTH = [("w", "weak"), ("s", "strong")]
+    hold = models.CharField(max_length=1, choices=HOLD_STRENGTH, default="w")
+    category = models.CharField(max_length=100, null=True, blank=True)
+    turf = ArrayField(models.CharField(max_length=100), default=list)
+    headquarters = models.CharField(max_length=100, null=True, blank=True)
+    assets: ArrayField(models.CharField(max_length=100))
+    quirks: models.CharField(null=True, blank=True)
 
 
-# class Faction(models.Model):
-# name (e.g. "the unseen")
-# tier (e.g. 0, 1, 2, 3, 4, 5)
-# hold (e.g. "strong", "weak")
-# category text (e.g. "underworld", "institutions", "labor and trade", "the fringe", "citizenry")
-# turf: [text] (e.g. ["stockyard", "slaughterhouse", "animal fighting pits"])
-# headquarters: text (e.g. "A butcher shop")
-# assets: [text] (e.g. "a large gang of bloodthirsty butchers", "a pack of death-dogs"])
-# quirks: text
+class District(models.Model):
+    name: models.CharField(max_length=100, null=True, blank=True)
+    description: models.CharField(null=True, blank=True)
+    scene: models.CharField(null=True, blank=True)
+    streets_description: models.CharField(null=True, blank=True)
+    streets: ArrayField(models.CharField(max_length=100))
+    buildings_description: models.CharField(null=True, blank=True)
+    traits: ArrayField(
+        ArrayField(models.CharField(max_length=100))
+    )  # [[trait, strength],]
 
 
-# class District(models.Model):
-# name: text (e.g. "barrowcleft", "brightstone", "charhollow", "charterhall", "coalridge", "crow's foot", "the docks", "dunslough", "nightmarket", "silkshore", "six towers", "whitecrown", "the lost district")
-# description: text
-# scene: text
-# streets: text
-# building: text
-# traits: [(text, int),] (e.g. [("WEALTH", 1), ("CRIME", 4)])
+class DistrictFaction(models.Model):
+    district: models.ForeignKey(District, on_delete=models.CASCADE)
+    faction: models.ForeignKey(Faction, on_delete=models.CASCADE)
 
-# class DistrictFaction(models.Model):
-# district: fk.District
-# faction: fk.Faction
 
 # join table, but this will require some constraints. Could do {source_faction, target_faction, relationship} and likely have duplicate
 # relationship entries, could do {faction_a, faction_b, relationship} but you'd need some way to say "get me faction_c's relationships" and pull
