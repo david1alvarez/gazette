e46762cd05f8a59b62316ea1063dc56628727a3b
David Alvarez <daveyalvarez03@gmail.com>
2023-05-11 19:06:22 -0700

    clean up migrations, models

--- city_manager/admin.py
+++ city_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    City,
+    District,
+    Faction,
+    DistrictFaction,
+    FactionFactionRelation,
+    Landmark,
+    NonPlayerCharacter,
+)
+
+admin.site.register(City)
+admin.site.register(Faction)
+admin.site.register(District)
+admin.site.register(DistrictFaction)
+admin.site.register(FactionFactionRelation)
+admin.site.register(Landmark)
+admin.site.register(NonPlayerCharacter)
--- city_manager/models.py
+++ city_manager/models.py
@@ -3,52 +3,58 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class City(models.Model):
-    name: models.CharField(max_length=100)
+    name = models.CharField(null=True, blank=True, max_length=100)
 
 
 class Faction(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(null=True, blank=True, max_length=100)
     tier = models.IntegerField(default=0)
     HOLD_STRENGTH = [("w", "weak"), ("s", "strong")]
     hold = models.CharField(max_length=1, choices=HOLD_STRENGTH, default="w")
     category = models.CharField(max_length=100, null=True, blank=True)
     turf = ArrayField(models.CharField(max_length=100), default=list)
     headquarters = models.CharField(max_length=100, null=True, blank=True)
-    assets: ArrayField(models.CharField(max_length=100))
-    quirks: models.CharField(null=True, blank=True)
-    city: models.ForeignKey(City, on_delete=models.PROTECT)
+    assets = ArrayField(models.CharField(max_length=100))
+    quirks = models.TextField(null=True, blank=True)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
 
 
 class District(models.Model):
-    name: models.CharField(max_length=100, null=True, blank=True)
-    description: models.CharField(null=True, blank=True)
-    scene: models.CharField(null=True, blank=True)
-    streets_description: models.CharField(null=True, blank=True)
-    streets: ArrayField(models.CharField(max_length=100))
-    buildings_description: models.CharField(null=True, blank=True)
-    traits: ArrayField(ArrayField(models.CharField(max_length=100)))
-    city: models.ForeignKey(City, on_delete=models.PROTECT)
+    name = models.CharField(null=True, blank=True, max_length=100)
+    description = models.TextField(null=True, blank=True)
+    scene = models.TextField(null=True, blank=True)
+    streets_description = models.TextField(null=True, blank=True)
+    streets = ArrayField(models.CharField(max_length=100))
+    buildings_description = models.TextField(null=True, blank=True)
+    traits = ArrayField(
+        ArrayField(models.CharField(max_length=100))
+    )  # this doesn't work in the current admin panel
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
 
 
 class DistrictFaction(models.Model):
-    district: models.ForeignKey(District, on_delete=models.CASCADE)
-    faction: models.ForeignKey(Faction, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    faction = models.ForeignKey(Faction, on_delete=models.CASCADE)
 
 
 class FactionFactionRelation(models.Model):
-    source_faction: models.ForeignKey(Faction, on_delete=models.CASCADE)
-    target_faction: models.ForeignKey(Faction, on_delete=models.CASCADE)
-    target_reputation: models.IntegerField(default=0)
+    source_faction = models.ForeignKey(
+        Faction, on_delete=models.CASCADE, related_name="source_faction"
+    )
+    target_faction = models.ForeignKey(
+        Faction, on_delete=models.CASCADE, related_name="target_faction"
+    )
+    target_reputation = models.IntegerField(default=0)
 
 
 class Landmark(models.Model):
-    name: models.CharField(max_length=100, null=True, blank=True)
-    description: models.CharField(null=True, blank=True)
-    district: models.ForeignKey(District, on_delete=models.PROTECT)
+    name = models.CharField(null=True, blank=True, max_length=100)
+    description = models.TextField(null=True, blank=True)
+    district = models.ForeignKey(District, on_delete=models.PROTECT)
 
 
 class NonPlayerCharacter(models.Model):
-    name: models.CharField(max_length=100, null=True, blank=True)
-    description: models.CharField(null=True, blank=True)
-    adjectives: ArrayField(models.CharField(max_length=100), default=list)
-    district: models.ForeignKey(District, on_delete=models.PROTECT)
+    name = models.CharField(null=True, blank=True, max_length=100)
+    description = models.TextField(null=True, blank=True)
+    adjectives = ArrayField(models.CharField(max_length=100), default=list)
+    district = models.ForeignKey(District, on_delete=models.PROTECT)
