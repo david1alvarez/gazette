2d61ce98636d29bc19bed873ea5ab2ae6eeb4c9f
David Alvarez <daveyalvarez03@gmail.com>
2023-05-14 14:18:42 -0700

    Add Faction Clock table

--- city_manager/admin.py
+++ city_manager/admin.py
@@ -4,6 +4,7 @@ from .models import (
     City,
     District,
     Faction,
+    FactionClock,
     DistrictFaction,
     FactionFactionRelation,
     Landmark,
@@ -13,34 +14,39 @@ from .models import (
 
 @admin.register(City)
 class AdminCity(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = ("name", "id")
 
 
 @admin.register(Faction)
 class AdminFaction(admin.ModelAdmin):
-    list_display = ("name", "tier", "hold", "city")
+    list_display = ("name", "tier", "hold", "city", "id")
+
+
+@admin.register(FactionClock)
+class AdminFactionClock(admin.ModelAdmin):
+    list_display = ("name", "faction", "objective_type", "id")
 
 
 @admin.register(District)
 class AdminDistrict(admin.ModelAdmin):
-    list_display = ("id", "name", "city")
+    list_display = ("name", "city", "id")
 
 
 @admin.register(DistrictFaction)
 class AdminDistrictFaction(admin.ModelAdmin):
-    list_display = ("id", "district", "faction")
+    list_display = ("district", "faction", "id")
 
 
 @admin.register(FactionFactionRelation)
 class AdminFactionFactionRelation(admin.ModelAdmin):
-    list_display = ("id", "source_faction", "target_faction", "target_reputation")
+    list_display = ("source_faction", "target_faction", "target_reputation", "id")
 
 
 @admin.register(Landmark)
 class AdminLandmark(admin.ModelAdmin):
-    list_display = ("id", "name", "district")
+    list_display = ("name", "district", "id")
 
 
 @admin.register(NonPlayerCharacter)
 class AdminNonPlayerCharacter(admin.ModelAdmin):
-    list_display = ("id", "name", "district", "faction")
+    list_display = ("name", "district", "faction", "id")
